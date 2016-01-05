670a10659c46cb9862a91e5b6128c7516b1722f7
FeelMix <Fee1Mix@163.com>
2016-01-06 01:20:40 +0800

    modified mail to

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,6 +1,6 @@
 class CommentMailer < ApplicationMailer
   def comment_notify_email(user)
     @user = user
-    mail to: "553815182@qq.com", subject: 'HTML 课程登录指南'
+    mail to: @user.email, subject: 'HTML 课程登录指南'
   end
 end
