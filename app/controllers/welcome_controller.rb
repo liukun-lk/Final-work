4ed8d08dee06eb37f83f3fb712e7ffaa7a935971
FeelMix <Fee1Mix@163.com>
2016-01-07 14:06:25 +0800

    modified test.csv

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
 
   end
   def new
-
+    User.where(id: ..108).each do |user|
+      CommentMailer.comment_notify_email(user).deliver_later
+    end
   end
 end
