f1dbfb04682e6a5d41e300bf01cc022382ceb7c0
FeelMix <Fee1Mix@163.com>
2016-01-07 14:50:36 +0800

    fix welcome new bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,6 @@ class WelcomeController < ApplicationController
 
   end
   def new
-    User.where(id: 38..108).each do |user|
-      CommentMailer.comment_notify_email(user).deliver_later
-    end
+   
   end
 end
