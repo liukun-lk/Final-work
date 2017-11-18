a39923a912837066f9b814a9eae8ce210c59bdad
feelmix <feelmix@feelmixdeMBP.lan>
2017-11-18 16:00:02 +0800

    add mail preview

--- test/mailers/previews/comment_mailer_preview.rb
+++ test/mailers/previews/comment_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/comment_mailer
 class CommentMailerPreview < ActionMailer::Preview
+  def comment_notify_email
+    CommentMailer.comment_notify_email(User.first)
+  end
 end
