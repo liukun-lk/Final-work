5405ea2a0eebeb0a7a466f7a54251db5287bae56
feelmix <feelmix@feelmixdeMBP.lan>
2018-02-03 16:24:46 +0800

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,75 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118073621) do
+ActiveRecord::Schema.define(version: 2017_11_18_073621) do
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "title",                   limit: 255
-    t.text     "description",             limit: 65535
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
-    t.string   "user_id",                 limit: 255
-    t.string   "image_file_name",         limit: 255
-    t.string   "image_content_type",      limit: 255
-    t.integer  "image_file_size",         limit: 4
+  create_table "projects", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.integer  "cached_votes_total",      limit: 4,     default: 0
-    t.integer  "cached_votes_score",      limit: 4,     default: 0
-    t.integer  "cached_votes_up",         limit: 4,     default: 0
-    t.integer  "cached_votes_down",       limit: 4,     default: 0
-    t.integer  "cached_weighted_score",   limit: 4,     default: 0
-    t.integer  "cached_weighted_total",   limit: 4,     default: 0
-    t.float    "cached_weighted_average", limit: 24,    default: 0.0
-    t.string   "zip_file_name",           limit: 255
-    t.string   "zip_content_type",        limit: 255
-    t.integer  "zip_file_size",           limit: 4
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
+    t.string "zip_file_name"
+    t.string "zip_content_type"
+    t.integer "zip_file_size"
     t.datetime "zip_updated_at"
+    t.index ["cached_votes_down"], name: "index_projects_on_cached_votes_down"
+    t.index ["cached_votes_score"], name: "index_projects_on_cached_votes_score"
+    t.index ["cached_votes_total"], name: "index_projects_on_cached_votes_total"
+    t.index ["cached_votes_up"], name: "index_projects_on_cached_votes_up"
+    t.index ["cached_weighted_average"], name: "index_projects_on_cached_weighted_average"
+    t.index ["cached_weighted_score"], name: "index_projects_on_cached_weighted_score"
+    t.index ["cached_weighted_total"], name: "index_projects_on_cached_weighted_total"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  add_index "projects", ["cached_votes_down"], name: "index_projects_on_cached_votes_down", using: :btree
-  add_index "projects", ["cached_votes_score"], name: "index_projects_on_cached_votes_score", using: :btree
-  add_index "projects", ["cached_votes_total"], name: "index_projects_on_cached_votes_total", using: :btree
-  add_index "projects", ["cached_votes_up"], name: "index_projects_on_cached_votes_up", using: :btree
-  add_index "projects", ["cached_weighted_average"], name: "index_projects_on_cached_weighted_average", using: :btree
-  add_index "projects", ["cached_weighted_score"], name: "index_projects_on_cached_weighted_score", using: :btree
-  add_index "projects", ["cached_weighted_total"], name: "index_projects_on_cached_weighted_total", using: :btree
-  add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "stu_id",                 limit: 255
-    t.string   "stu_name",               limit: 255
-    t.string   "random_pass",            limit: 255
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "stu_id"
+    t.string "stu_name"
+    t.string "random_pass"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["stu_id"], name: "index_users_on_stu_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["stu_id"], name: "index_users_on_stu_id", using: :btree
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id",   limit: 4
-    t.string   "votable_type", limit: 255
-    t.integer  "voter_id",     limit: 4
-    t.string   "voter_type",   limit: 255
-    t.boolean  "vote_flag"
-    t.string   "vote_scope",   limit: 255
-    t.integer  "vote_weight",  limit: 4
+  create_table "votes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.integer "votable_id"
+    t.string "votable_type"
+    t.integer "voter_id"
+    t.string "voter_type"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
 end
